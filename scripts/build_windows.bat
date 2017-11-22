4a7eedd684de70a7ebbdadf44110bd5b18cd5bc1
Tobias Zimmermann <tobias.zimmermann@cs.uni-kl.de>
2017-11-22 16:24:23 +0100

    Updated batch file with library path
    Signed-off-by: Tobias Zimmermann <tobias.zimmermann@cs.uni-kl.de>

--- scripts/build_windows.bat
+++ scripts/build_windows.bat
@@ -57,18 +57,27 @@ cmake .. ^
   -DUSE_CUDA=%USE_CUDA% ^
   -DUSE_NNPACK=OFF ^
   -DUSE_GLOG=OFF ^
-  -DUSE_GFLAGS=OFF ^
+  -DUSE_GFLAGS=ON ^
   -DUSE_LMDB=ON ^
   -DUSE_LEVELDB=OFF ^
   -DUSE_ROCKSDB=OFF ^
   -DUSE_OPENCV=OFF ^
   -DBUILD_SHARED_LIBS=OFF ^
-  -DBUILD_PYTHON=ON ^
+  -DBUILD_PYTHON=OFF ^
   -DBUILD_BINARY=OFF ^
-  -DCAFFE2_CUSTOM_PROTOC_EXECUTABLE=%CAFFE2_ROOT%\build_host_protoc\bin\protoc.exe ^
-  -DLMDB_DIR=C:\Users\tozim\source\lmdb.v140.0.9.19.3\build\native ^
-  -DLMDB_INCLUDE_DIR=C:\\Users\\tozim\\source\\lmdb.v140.0.9.19.3\\build\\native\\include ^
-  -DLMDB_LIBRARIES=C:\Users\tozim\source\lmdb.v140.0.9.19.3\build\native\lib\x64\Release\lmdb.lib ^
+  -DCAFFE2_CUSTOM_PROTOC_EXECUTABLE=C:\\bin\\protoc.exe ^
+  -DProtobuf_INCLUDE_DIRS=C:\\include ^
+  -DProtobuf_LIBRARIES=C:\\lib\\libprotobuf.lib ^
+  -DProtobuf_PROTOC_LIBRARIES=C:\\lib\\libprotoc.lib ^
+  -DProtobuf_LITE_LIBRARIES=C:\\lib\\libprotobuf-lite.lib ^
+  -DLMDB_DIR=C:\\ ^
+  -DLMDB_INCLUDE_DIR=C:\\include ^
+  -DLMDB_LIBRARIES=C:\\lib\\lmdb.lib ^
+  -DGFLAGS_ROOT_DIR=C:\\ ^
+  -DGFLAGS_INCLUDE_DIRS=C:\\include ^
+  -DGFLAGS_LIBRARIES=C:\\lib\\gflags_static.lib ^
+  -DGFLAGS_LIBRARYRARY_DIRS=C:\\lib ^
+  -Dpybind11_INCLUDE_DIRS=C:\\include ^
   || goto :label_error
 
 :: Actually run the build
